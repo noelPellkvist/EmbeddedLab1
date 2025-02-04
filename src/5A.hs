d0369c46e90b5be334532e8b909eb5557f0cb92b
noelp <noelp@kth.se>
2025-02-04 19:19:40 +0100

    5 probably done, maybe need to fix the select

--- src/5A.hs
+++ src/5A.hs
@@ -1,11 +1,38 @@
 import ForSyDe.Shallow
 
-switch :: Signal Int -> Signal Bool -> (Signal Int, Signal Int)
-switch s_in s_bool = (s_out_1, s_out_2)
+selectSystem :: Signal Int -> Signal Int -> Signal Bool -> Signal Int
+selectSystem s_t s_f s_bool = s_out
+  where
+    c_1  = d_select s_bool
+    s_out = kernel21SADF c_1 s_t s_f
+
+d_select = detector11SADF consume_rate next_state select_scenario initial_state
+  where
+    consume_rate = 1
+
+    next_state _ [b] = if b then 0 else 1
+
+    scenario_0 = ( (1, 0)
+                 , 1
+                 , \ts fs -> ts   -- read from the first input
+                 )
+
+    scenario_1 = ( (0, 1)
+                 , 1
+                 , \ts fs -> fs   -- read from the second input
+                 )
+
+    select_scenario 0 = (1, [scenario_0])
+    select_scenario 1 = (1, [scenario_1])
+
+    initial_state = 0
+
+switchSystem :: Signal Int -> Signal Bool -> (Signal Int, Signal Int)
+switchSystem s_in s_bool = (s_out_1, s_out_2)
   where
     c_1 = d_switch s_bool
 
-    (s_out_1, s_out_2) = k_1 c_1 s_in
+    (s_out_1, s_out_2) = kernel12SADF c_1 s_in
 
 d_switch = detector11SADF consume_rate next_state select_scenario initial_state
   where
@@ -21,14 +48,22 @@ d_switch = detector11SADF consume_rate next_state select_scenario initial_state
 
     initial_state = 0
 
-k_1 = kernel12SADF
 
 main :: IO ()
-main = do 
-  let s_in   = takeS 10 $ infiniteS (+1) 0
-  let s_bool = signal [True, True, True, True, True, False, True, True, True, True]
-  let (s_out_1, s_out_2) = switch s_in s_bool
-  putStrLn "=== s_out_1 ==="
-  print s_out_1
-  putStrLn "=== s_out_2 ==="
-  print s_out_2
+main = do
+  let s_t    = signal [1,2,3,4,5]
+  let s_f    = signal [6,7,8,9,10]
+  let s_bool = signal [True, True, False, False, True]
+
+  let s_out_select  = selectSystem s_t s_f s_bool
+  putStrLn "== Output from select =="
+  print s_out_select
+
+  let s_in    = signal [1,2,3,4,5]
+  let s_bool = signal [True, True, False, False, True]
+
+  let (s_out_switch_1, s_out_switch_2)  = switchSystem s_in s_bool
+  putStrLn "== Output from switch (T) =="
+  print s_out_switch_1
+  putStrLn "== Output from switch (F) =="
+  print s_out_switch_2
